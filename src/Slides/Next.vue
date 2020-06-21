5a492714c3591fa56cefda45edf2fb5dc46e6284
jsusilo <enr@bk.ru>
2020-06-21 23:45:21 +0500

    +slides

---
+++ src/Slides/Next.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="slide-wrapper">
+    <h1>
+      Нефть Brent может завершить неделю ростом и сформировать «бычье
+      поглощение»
+    </h1>
+    <p>На рынке появились сигналы оживления спрос на нефтепродукты.</p>
+    <p>Риски второй волны пандемии оказывают сдерживающий эффект на цены. </p>
+    <img src="../assets/557631-1.jpeg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Start",
+};
+</script>
+
+<style scoped>
+img {
+  width: 70%;
+}
+</style>
--- src/Slides/index.js
+++ src/Slides/index.js
@@ -1,10 +1,14 @@
 
 import Start from './Start.vue';
 import Flow from './Flow.vue';
+import Next from './Next.vue';
+import NextNext from './NextNext.vue';
 import End from './End.vue';
 
 export default [
     Flow,
+    Next,
+    NextNext,
     Start,
     End
 ];
