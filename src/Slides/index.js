31ac9c181794e8e4d3003cf3ea343b0aeb2f48b7
jsusilo <enr@bk.ru>
2020-06-22 00:02:03 +0500

    фввшешщтфд slides

--- src/Slides/index.js
+++ src/Slides/index.js
@@ -2,12 +2,14 @@
 import Start from './Start.vue';
 import Flow from './Flow.vue';
 import Next from './Next.vue';
+import Next2 from './Next2.vue';
 import NextNext from './NextNext.vue';
 import End from './End.vue';
 
 export default [
     Flow,
     Next,
+    Next2,
     NextNext,
     Start,
     End
