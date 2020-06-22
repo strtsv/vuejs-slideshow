856be4104b89de3895fbe7791c252a016f851ea7
jsusilo <enr@bk.ru>
2020-06-23 00:07:21 +0500

    + slides

---
+++ src/Slides/Next1.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="slide-wrapper">
+    <h1>
+      Morgan Stanley ждет фронтального снижения доллара США
+    </h1>
+    <p>Особенно банк выделяет австралийский доллар, так как местная экономика будет опережать аналогичные в ходе восстановления после пандемии</p>
+    <img src="../assets/557667-1.jpeg" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Next1",
+};
+</script>
+
+<style scoped>
+img {
+  width: 70%;
+}
+</style>
--- src/Slides/index.js
+++ src/Slides/index.js
@@ -2,6 +2,7 @@
 import Start from './Start.vue';
 import Flow from './Flow.vue';
 import Next from './Next.vue';
+import Next1 from './Next1.vue';
 import Next2 from './Next2.vue';
 import NextNext from './NextNext.vue';
 import End from './End.vue';
@@ -9,6 +10,7 @@ import End from './End.vue';
 export default [
     Flow,
     Next,
+    Next1,
     Next2,
     NextNext,
     Start,
